41e9135d573d6b1e356d1f3b51a98a0e7c8c8ed0
niehaixingchen <niehaixingchen@163.com>
2020-02-25 23:38:23 +0800

    done

---
+++ category.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+<div class="post container">
+    <?php
+    echo "分类： " . single_cat_title();
+    if (have_posts()) {
+        //获取下一篇文章的信息，并且将信息存入全局变量 $post 中
+        the_post();
+        get_template_part("template-parts/post", "item");
+    ?>
+
+    <?php
+    } else {
+        echo '没有日志可以显示';
+    }
+    posts_nav_link();
+    ?>
+</div>
+
+<?php get_footer(); ?>
--- index.php
+++ index.php
@@ -5,27 +5,15 @@
         while (have_posts()) {
             //获取下一篇文章的信息，并且将信息存入全局变量 $post 中
             the_post();
+            get_template_part("template-parts/post", "item");
     ?>
-            <div class="row">
-                <div class="post-item col-lg border">
-                    <div class="post-title">
-                        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                            <h2>
-                    </div>
-                    <div class="post-content"><?php the_content(); ?></div>
-                    <div class="post-meta">
-                        <?php the_category(','); ?>
-                        <?php the_author(); ?>
-                        <?php the_time('Y-m-d'); ?>
-                        <?php edit_post_link(' Edit ', ' <span>|</span> ', ''); ?>
-                    </div>
-                </div>
-            </div>
+         
     <?php
         }
     } else {
         echo '没有日志可以显示';
     }
+    posts_nav_link();
     ?>
 </div>
 
